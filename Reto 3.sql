450dbe9dd6d1398dfbcb31d3d13a3848878d8a5a
Armando <armandoramossanchez@gmail.com>
2021-10-14 18:47:46 +0200

    subo bbdd reto 3

---
+++ Reto 3.sql
@@ -0,0 +1,36 @@
+create database Reto_3;
+
+use Reto_3;
+
+create table Comunidades_Autonomas(
+id int not null auto_increment,
+nombre varchar(50),
+poblacion int,
+superficie int,
+capital int,
+primary key (id)
+);
+
+create table Provincias(
+id int not null auto_increment,
+nombre varchar(50),
+superficie int,
+capital int,
+id_Comunidad_Autonoma int,
+constraint FK_CA foreign key (id_Comunidad_Autonoma) references Comunidades_Autonomas (id),
+primary key (id)
+);
+
+create table Localidades(
+id int not null auto_increment,
+nombre varchar(50),
+población int,
+id_provincia int,
+constraint FK_Provincia foreign key (id_provincia) references Provincias (id),
+primary key (id)
+);
+
+
+
+
+
